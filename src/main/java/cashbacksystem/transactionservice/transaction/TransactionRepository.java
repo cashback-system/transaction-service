faa691f2520044b479f07ec11174df733c69235a
DaPiankov <DaPiankov@croc.ru>
2024-02-14 17:26:56 +0300

    [feat]: Добавил функционал по поиску транзакций по параметрам

--- src/main/java/cashbacksystem/transactionservice/transaction/TransactionRepository.java
+++ src/main/java/cashbacksystem/transactionservice/transaction/TransactionRepository.java
@@ -2,8 +2,22 @@ package cashbacksystem.transactionservice.transaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository
     extends JpaRepository<TransactionEntity, UUID> {
+
+    /**
+     * Найти все транзакции по карте и временному промежутку.
+     *
+     * @param cardId - идентификатор карты
+     * @param start  - минимальное время транзакции
+     * @param end    - максимальное время транзакции
+     * @return список транзакций, удовлетворяющих параметрам
+     */
+    List<TransactionEntity> findAllByCardIdAndTimeBetween(UUID cardId,
+                                                          ZonedDateTime start,
+                                                          ZonedDateTime end);
 }
